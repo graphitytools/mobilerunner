0a7c6364db7d195903928e3bb9b9de8a48bd8e29
Hrithik Tiwari <javaoncloud14@gmail.com>
2025-08-14 19:35:34 +0530

    init

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2292802.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 243935559, 243935558, 243935383, 243935560, 243935389, 243935404, 243935346, 243935388, 243935384, 243935352, 243935379, 243935556, 243935391, 243935500, 243935350, 243935345, 244981896, 244981897, 245021018, 245021019 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
